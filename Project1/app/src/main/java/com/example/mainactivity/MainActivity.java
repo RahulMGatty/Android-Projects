b0cdb6662dd37b9e35f62d2170afb3d9f6ccdd3b
RahulMGatty <rahulchad34@gmail.com>
2025-04-28 22:02:21 +0530

    initial commit

---
+++ Project1/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.mainactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        Log.d("Lifecycle","onCreate Method");
+
+        b.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, Second.class);
+            startActivity(i);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
