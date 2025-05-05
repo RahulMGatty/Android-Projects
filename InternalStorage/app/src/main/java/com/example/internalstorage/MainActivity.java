4a3c87a231e2f65d743c337d86faba3175fa534b
RahulMGatty <rahulchad34@gmail.com>
2025-05-05 15:31:59 +0530

    initial commit

--- InternalStorage/app/src/main/java/com/example/internalstorage/MainActivity.java
+++ InternalStorage/app/src/main/java/com/example/internalstorage/MainActivity.java
@@ -11,9 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String FILE_NAME = "example.txt";
@@ -36,15 +39,41 @@ public class MainActivity extends AppCompatActivity {
             editText.getText().clear();
             Toast.makeText(this, "Saved to:"+getFilesDir()+"/"+FILE_NAME,
                     Toast.LENGTH_SHORT).show();
-        }   catch(FileNotFoundException e){
+        } catch(FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }   finally{
+        } finally {
             try{
                 fos.close();
             }  catch (IOException e){
                 e.printStackTrace();
+            }
+        }
+    }
+    public void loadData(View view){
+        FileInputStream fis = null;
+        try{
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while((text = br.readLine())!=null){
+                sb.append(text).append("\n");
+            }
+            editText.setText(sb.toString());
+        }  catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }  catch (IOException e){
+            e.printStackTrace();
+        }  finally {
+            if(fis !=null){
+                try{
+                    fis.close();
+                }   catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
     }
