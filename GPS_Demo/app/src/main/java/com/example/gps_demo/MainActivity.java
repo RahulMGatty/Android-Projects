72c6433ef603c2b2dd08e8a4059d3fbf139daa1e
RahulMGatty <rahulchad34@gmail.com>
2025-05-08 11:06:13 +0530

    initial build

--- GPS_Demo/app/src/main/java/com/example/gps_demo/MainActivity.java
+++ GPS_Demo/app/src/main/java/com/example/gps_demo/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.gps_demo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager manager;
+    LocationListener listener;
+
+    HashMap<String,String> hashMapData;
+
+    ArrayList<HashMap<String,String>> datalist;
+
+    ListView gpsListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        manager =(LocationManager) getSystemService(LOCATION_SERVICE);
+        datalist = new ArrayList<HashMap<String, String>>();
+        gpsListView =findViewById(R.id.lvGPS);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                updateMyLocation(location);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                manager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,0,listener);
+                Location lastLocation = manager.getLastKnownLocation
+                        (LocationManager.GPS_PROVIDER);
+                updateMyLocation(lastLocation);
+            } else {
+                requestPermissions(new String[]
+                        {Manifest.permission.ACCESS_FINE_LOCATION},
+                        123);
+            }
+        }
     }
+
+
 }
