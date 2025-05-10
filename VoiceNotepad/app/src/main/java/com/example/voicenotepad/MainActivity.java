0126a5d08dd485b52c1865e20181d41dda504711
RahulMGatty <rahulchad34@gmail.com>
2025-05-10 12:17:54 +0530

    intital commit

--- VoiceNotepad/app/src/main/java/com/example/voicenotepad/MainActivity.java
+++ VoiceNotepad/app/src/main/java/com/example/voicenotepad/MainActivity.java
@@ -1,24 +1,103 @@
 package com.example.voicenotepad;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtContent;
+
+    Button btnConvert;
+
+    ImageButton btnSpeak;
+
+    TextView txtChars;
+
+    TextToSpeech tts;
+    protected static final int RESULT_SPEECH =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtContent = findViewById(R.id.editMultiLine);
+        btnConvert = findViewById(R.id.btnConvert);
+        btnSpeak = findViewById(R.id.imgButton);
+        txtChars = findViewById(R.id.txtChars);
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts = new TextToSpeech(getApplicationContext(),
+                        new TextToSpeech.OnInitListener() {
+                            @Override
+                            public void onInit(int status) {
+                                if (status == TextToSpeech.SUCCESS) {
+                                    tts.setLanguage(Locale.US);
+                                    tts.setSpeechRate(1.0f);
+                                    tts.speak(txtContent.getText().toString(),
+                                            TextToSpeech.QUEUE_ADD, null);
+
+                                }
+                            }
+                        });
+
+            }
         });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        "en-US");
+
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtChars.setText("");
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Your Device " +
+                            "Doesnt support speech recognition", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case RESULT_SPEECH:
+                if(resultCode==RESULT_OK && data != null){
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtChars.setText(text.get(0));
+                }
+                break;
+        }
     }
 }
